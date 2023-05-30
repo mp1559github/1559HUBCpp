a635d79c009b37e0c0f1cee26819b50276c285b7
mp1559github <134990483+mp1559github@users.noreply.github.com>
2023-05-30 00:23:58 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+  string username;
+  string password;
+
+  do {
+    cout << "Username: " << end1;
+    cin >> username;
+    
+   cout << "Password" << end1;
+    cin >> password;
+
+    if (username == "Admin")
+    {
+      if (password == "Password")
+    }
+    cout << "_________________________"<< end1;
+    cout << "You have successfully logged in" << end1;
+    cout << "______________________________" << end1;
+  }
+    else{
+    cout << "______________________________" << end1;
+    cout << "You've entered incorrect login details." << end1;
+    cout << "______________________________" << end1;
+    }
+}
+else {
+  cout << "_________________________" << end1;
+  cout << "You've entered incorrect login details." << end1;
+  cout << "_________________________" << end1;
+}
+  
+while (username != "Admin" || password != "Password");
+    
+}
+    
